d5c72525f1a9947b5a9edf4be2b2999d61cff7d2
LiJi <leegere@gmail.com>
2013-03-25 20:43:09 +0800

    test4 6th commit

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,31 +3,35 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "RoR Sample"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
-      response.should have_selector("title", :content => "RoR Sample| Home")
+      response.should have_selector("title", :content => @base_title + "| Home")
     end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
-      response.should have_selector("title", :content => "RoR Sample| Contact")
+      response.should have_selector("title", :content => @base_title + "| Contact")
     end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
-      response.should have_selector("title", :content => "RoR Sample| About")
+      response.should have_selector("title", :content => @base_title + "| About")
     end
   end
 
   describe "GET 'help'" do
     it "should be successful" do
       get 'help'
-      response.should have_selector("title", :content => "RoR Sample| Help")
+      response.should have_selector("title", :content => @base_title + "| Help")
     end
   end
 end
